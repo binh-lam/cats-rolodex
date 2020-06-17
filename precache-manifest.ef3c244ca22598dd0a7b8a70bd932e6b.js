42ee91948163c9e6075d59c39090501702d16901
binhlam-skip <binh.lam@skipthedishes.ca>
2020-06-17 16:59:07 -0500

    Updates

---
+++ precache-manifest.ef3c244ca22598dd0a7b8a70bd932e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77968a2497de6e60ceace2f48848de4",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "9f46e8b4f7b2aa3147c0",
+    "url": "/cats-rolodex/static/css/main.c0e6711a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f46e8b4f7b2aa3147c0",
+    "url": "/cats-rolodex/static/js/main.a5261244.chunk.js"
+  },
+  {
+    "revision": "f3619f5b32a2d9ecc6f2",
+    "url": "/cats-rolodex/static/js/runtime-main.f5fe2f9e.js"
+  }
+]);
